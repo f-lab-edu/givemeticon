b58c1e2e68d348de666cf05bebe59f5897e9a478
jinhyuck <gjwlsgur4866@gmail.com>
2023-09-05 13:44:21 +0900

    [#4] feat : 프로젝트 생성

---
+++ src/test/java/com/example/givemeticon/GivemeticonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.givemeticon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GivemeticonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
