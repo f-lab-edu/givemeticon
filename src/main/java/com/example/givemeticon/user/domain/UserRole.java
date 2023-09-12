b17e3dbfe32eddcb277a9d9c23835653b6cad6ad
jinhyuck <gjwlsgur4866@gmail.com>
2023-09-12 14:13:04 +0900

    [#9] refactor : 불필요한 이름(설명) 제거

--- src/main/java/com/example/givemeticon/user/domain/UserRole.java
+++ src/main/java/com/example/givemeticon/user/domain/UserRole.java
@@ -1,11 +1,5 @@
 package com.example.givemeticon.user.domain;
 
 public enum UserRole {
-    USER("ROLE_USER"),
-    VIP("ROLE_VIP");
-    private final String name;
-
-    UserRole(String name) {
-        this.name = name;
-    }
+    USER, VIP
 }
