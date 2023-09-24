ce47f93a4bc756a1b27c737f18e682464293c9dd
jinhyuck <gjwlsgur4866@gmail.com>
2023-09-24 18:19:17 +0900

    [#14] feat : 토큰 기반 로그인 인증처리 추가

--- src/main/java/com/jinddung2/givemeticon/oauth/infrastructure/JwtTokenProvider.java
+++ src/main/java/com/jinddung2/givemeticon/oauth/infrastructure/JwtTokenProvider.java
@@ -1,11 +1,9 @@
 package com.jinddung2.givemeticon.oauth.infrastructure;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +11,7 @@ import java.security.Key;
 import java.util.Date;
 
 @Component
+@Slf4j
 public class JwtTokenProvider {
     private final Key key;
 
@@ -32,6 +31,22 @@ public class JwtTokenProvider {
     public String extractSubject(String accessToken) {
         Claims claims = parseClaims(accessToken);
         return claims.getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.warn("Invalid JWT Token", e);
+        } catch (ExpiredJwtException e) {
+            log.warn("Expired JWT Token", e);
+        } catch (UnsupportedJwtException e) {
+            log.warn("Unsupported JWT Token", e);
+        } catch (IllegalArgumentException e) {
+            log.warn("JWT claims string is empty.", e);
+        }
+        return false;
     }
 
     private Claims parseClaims(String accessToken) {
