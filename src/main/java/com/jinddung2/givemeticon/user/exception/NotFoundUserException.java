5d31adf73270ed040bdb8fdb4a2aef83b4b88d8a
jinhyuck <gjwlsgur4866@gmail.com>
2023-09-26 13:11:44 +0900

    [#20] feat : 유저 예외처리

---
+++ src/main/java/com/jinddung2/givemeticon/user/exception/NotFoundUserException.java
@@ -0,0 +1,9 @@
+package com.jinddung2.givemeticon.user.exception;
+
+import com.jinddung2.givemeticon.common.exception.ErrorCode;
+
+public class NotFoundUserException extends UserException {
+    public NotFoundUserException() {
+        super(ErrorCode.NOT_FOUND_USER);
+    }
+}
