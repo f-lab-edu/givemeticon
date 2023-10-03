7ec23998769526a0eabba934f1e20d74d88e6365
jinhyuck <gjwlsgur4866@gmail.com>
2023-10-03 19:52:34 +0900

    [#30] test : 브랜드 이름 변경 테스트

--- src/test/java/com/jinddung2/givemeticon/brand/presentation/BrandControllerTest.java
+++ src/test/java/com/jinddung2/givemeticon/brand/presentation/BrandControllerTest.java
@@ -2,12 +2,16 @@ package com.jinddung2.givemeticon.brand.presentation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jinddung2.givemeticon.brand.application.BrandService;
+import com.jinddung2.givemeticon.brand.exception.DuplicatedBrandNameException;
+import com.jinddung2.givemeticon.brand.exception.NotFoundBrandException;
 import com.jinddung2.givemeticon.brand.presentation.request.BrandCreateRequest;
+import com.jinddung2.givemeticon.brand.presentation.request.BrandUpdateNameRequest;
 import com.jinddung2.givemeticon.common.config.WebConfig;
 import com.jinddung2.givemeticon.common.interceptor.AuthInterceptor;
 import com.jinddung2.givemeticon.user.application.LoginService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -15,8 +19,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(value = BrandController.class,
@@ -35,10 +41,12 @@ class BrandControllerTest {
     BrandService brandService;
 
     BrandCreateRequest brandCreateRequest;
+    BrandUpdateNameRequest brandUpdateNameRequest;
 
     @BeforeEach
     void setUp() {
         brandCreateRequest = new BrandCreateRequest(100, "testBrand");
+        brandUpdateNameRequest = new BrandUpdateNameRequest("updateNameTest");
     }
 
     @Test
@@ -48,5 +56,44 @@ class BrandControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(brandCreateRequest)))
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    void brand_Create_Fail_Exists_Brand_Name() throws Exception {
+        Mockito.doThrow(new DuplicatedBrandNameException()).when(brandService).save(brandCreateRequest);
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/brands")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(brandCreateRequest)))
+                .andExpect(status().isBadRequest());
+
+        resultActions
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("이미 존재하는 브랜드입니다."));
+    }
+
+    @Test
+    void brand_Update_Name_Success() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/api/v1/brands/100")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(brandUpdateNameRequest)))
+                .andExpect(status().isOk());
+
+        Mockito.verify(brandService).updateName(100, brandUpdateNameRequest.name());
+    }
+
+    @Test
+    void brand_Update_Name_Fail_Not_Found_Brand() throws Exception {
+        Mockito.doThrow(new NotFoundBrandException()).when(brandService).updateName(100, brandUpdateNameRequest.name());
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+                        .put("/api/v1/brands/100")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(brandUpdateNameRequest)))
+                .andExpect(status().isBadRequest());
+
+        resultActions
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("존재하지 않는 브랜드입니다."));
     }
 }
