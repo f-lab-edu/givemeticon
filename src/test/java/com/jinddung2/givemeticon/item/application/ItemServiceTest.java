7523409393fe859be3fd3688f58128a68da4ead3
jinhyuck <gjwlsgur4866@gmail.com>
2023-10-06 20:59:45 +0900

    [#36] test : 아이템 생성 기능 테스트

---
+++ src/test/java/com/jinddung2/givemeticon/item/application/ItemServiceTest.java
@@ -0,0 +1,37 @@
+package com.jinddung2.givemeticon.item.application;
+
+import com.jinddung2.givemeticon.item.domain.Item;
+import com.jinddung2.givemeticon.item.infrasturcture.mapper.ItemMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ItemServiceTest {
+
+    @InjectMocks
+    ItemService itemService;
+
+    @Mock
+    ItemMapper itemMapper;
+
+    Item item;
+
+    @BeforeEach
+    void setUp() {
+        item = Item.builder()
+                .name("testName")
+                .price(10000)
+                .build();
+    }
+
+    @Test
+    void save_Success() {
+        itemService.save(item);
+        Mockito.verify(itemMapper).save(item);
+    }
+}
---
+++ src/test/java/com/jinddung2/givemeticon/item/presentation/ItemControllerTest.java
@@ -0,0 +1,58 @@
+package com.jinddung2.givemeticon.item.presentation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jinddung2.givemeticon.common.config.WebConfig;
+import com.jinddung2.givemeticon.common.interceptor.AuthInterceptor;
+import com.jinddung2.givemeticon.item.facade.ItemCreationFacade;
+import com.jinddung2.givemeticon.item.presentation.request.ItemCreateRequest;
+import com.jinddung2.givemeticon.user.application.LoginService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(value = ItemController.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {
+                WebConfig.class,
+                AuthInterceptor.class,
+                LoginService.class
+        }))
+class ItemControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    ItemCreationFacade itemCreationFacade;
+
+    ItemCreateRequest itemCreateRequest;
+    int brandId = 10;
+
+    @BeforeEach
+    void setUp() {
+        itemCreateRequest = new ItemCreateRequest("testName", 10000);
+    }
+
+    @Test
+    void createItem() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/items/brand/" + brandId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(itemCreateRequest)))
+                .andExpect(status().isCreated());
+
+        Mockito.verify(itemCreationFacade).createItem(brandId, itemCreateRequest);
+    }
+}
