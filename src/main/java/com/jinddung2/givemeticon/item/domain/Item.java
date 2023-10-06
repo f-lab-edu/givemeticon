ca909c51e09110df090b890e88228f9d09ee180a
jinhyuck <gjwlsgur4866@gmail.com>
2023-10-06 16:44:17 +0900

    [#36] feat : 아이템 및 아이템 세부사항 도메인 추가

---
+++ src/main/java/com/jinddung2/givemeticon/item/domain/Item.java
@@ -0,0 +1,38 @@
+package com.jinddung2.givemeticon.item.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class Item {
+
+    private int id;
+    private int brandId;
+    private String name;
+    private int price;
+    private int view_count;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+    @Builder
+    public Item(int id,
+                int brandId,
+                String name,
+                int price,
+                int view_count,
+                LocalDateTime createdDate,
+                LocalDateTime updatedDate
+    ) {
+        this.id = id;
+        this.brandId = brandId;
+        this.name = name;
+        this.price = price;
+        this.view_count = view_count;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+    }
+}
---
+++ src/main/java/com/jinddung2/givemeticon/item/domain/ItemDetail.java
@@ -0,0 +1,44 @@
+package com.jinddung2.givemeticon.item.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class ItemDetail {
+    private int id;
+    private int itemId;
+    private int sellerId;
+    private LocalDate expirationDate;
+    private boolean isUsed;
+    private LocalDate isUsedDate;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+    private LocalDateTime deletedDate;
+
+    @Builder
+    public ItemDetail(int id,
+                      int itemId,
+                      int sellerId,
+                      LocalDate expirationDate,
+                      boolean isUsed,
+                      LocalDate isUsedDate,
+                      LocalDateTime createdDate,
+                      LocalDateTime updatedDate,
+                      LocalDateTime deletedDate
+    ) {
+        this.id = id;
+        this.itemId = itemId;
+        this.sellerId = sellerId;
+        this.expirationDate = expirationDate;
+        this.isUsed = isUsed;
+        this.isUsedDate = isUsedDate;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+        this.deletedDate = deletedDate;
+    }
+}
