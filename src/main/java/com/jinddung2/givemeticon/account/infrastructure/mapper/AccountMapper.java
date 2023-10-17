1924dcaa8f66e2303ab179214ba4cf740815bd6a
jinhyuck <gjwlsgur4866@gmail.com>
2023-10-17 16:14:17 +0900

    [#26] feat : 계좌 인증 기능

--- src/main/java/com/jinddung2/givemeticon/account/infrastructure/mapper/AccountMapper.java
+++ src/main/java/com/jinddung2/givemeticon/account/infrastructure/mapper/AccountMapper.java
@@ -1,7 +1,14 @@
 package com.jinddung2.givemeticon.account.infrastructure.mapper;
 
+import com.jinddung2.givemeticon.account.domain.Account;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Optional;
 
 @Mapper
 public interface AccountMapper {
+
+    int save(Account account);
+
+    Optional<Account> findByTransactionId(String transactionId);
 }
--- src/main/java/com/jinddung2/givemeticon/account/presentation/OpenBankingController.java
+++ src/main/java/com/jinddung2/givemeticon/account/presentation/OpenBankingController.java
@@ -1,25 +1,49 @@
 package com.jinddung2.givemeticon.account.presentation;
 
-import com.jinddung2.givemeticon.account.application.openBankingService;
+import com.jinddung2.givemeticon.account.application.OpenBankingService;
+import com.jinddung2.givemeticon.account.infrastructure.BankCode;
 import com.jinddung2.givemeticon.account.presentation.response.OpenBankingToken;
 import com.jinddung2.givemeticon.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.NoSuchAlgorithmException;
 
 @RestController
 @RequestMapping("/api/v1/accounts")
 @RequiredArgsConstructor
+@Slf4j
 public class OpenBankingController {
 
-    private final openBankingService openBankingService;
+    private final OpenBankingService openBankingService;
 
     @PostMapping("/token")
     public ResponseEntity<ApiResponse<OpenBankingToken>> requestToken() {
         OpenBankingToken openBankingToken = openBankingService.requestAccessToken();
         return new ResponseEntity<>(ApiResponse.success(openBankingToken), HttpStatus.OK);
     }
+
+    @PostMapping("/real-name")
+    public ResponseEntity<ApiResponse<Integer>> requestMatchRealName(@RequestParam(name = "access_token") String accessToken,
+                                                                     @RequestParam(name = "transaction_id") String transactionId,
+                                                                     @RequestParam(name = "bank_code") BankCode bankCode,
+                                                                     @RequestParam(name = "account_num") String accountNum,
+                                                                     @RequestParam(name = "real_name") String realName,
+                                                                     @RequestParam(name = "birthday") String birthday)
+            throws JSONException, NoSuchAlgorithmException {
+        log.info("OpenBankingController.requestMatchRealName(): access_token={}, transactionId={}, bankCode={}, accountNum={}, realName={}, birthday={}",
+                accessToken, transactionId, bankCode, accountNum, realName, birthday);
+        int id = openBankingService.requestMatchRealName
+                (accessToken, transactionId, bankCode.getCode(), accountNum, realName, birthday);
+
+        return new ResponseEntity<>(ApiResponse.success(id), HttpStatus.CREATED);
+    }
+
 }
