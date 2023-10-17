05a878e452231e013c1d42918dcfd9dfd203084d
jinhyuck <gjwlsgur4866@gmail.com>
2023-10-17 17:38:49 +0900

    [#26] remove : 계좌 생성 로직 변경으로 인한 제거

--- src/main/java/com/jinddung2/givemeticon/account/exception/MisMatchBank.java
+++
@@ -1,9 +0,0 @@
-package com.jinddung2.givemeticon.account.exception;
-
-import com.jinddung2.givemeticon.common.exception.ErrorCode;
-
-public class MisMatchBank extends AccountException {
-    public MisMatchBank() {
-        super(ErrorCode.MISMATCH_BANK_CODE);
-    }
-}
