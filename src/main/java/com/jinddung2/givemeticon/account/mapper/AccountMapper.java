7250bfb2d77036e62d747518572d823388e12d62
jinhyuck <gjwlsgur4866@gmail.com>
2023-10-18 18:16:26 +0900

    [#26] feat : 계좌 생성 기능 추가

--- src/main/java/com/jinddung2/givemeticon/account/mapper/AccountMapper.java
+++ src/main/java/com/jinddung2/givemeticon/account/mapper/AccountMapper.java
@@ -1,14 +1,12 @@
-package com.jinddung2.givemeticon.account.infrastructure.mapper;
+package com.jinddung2.givemeticon.account.mapper;
 
 import com.jinddung2.givemeticon.account.domain.Account;
 import org.apache.ibatis.annotations.Mapper;
-
-import java.util.Optional;
 
 @Mapper
 public interface AccountMapper {
 
     int save(Account account);
 
-    Optional<Account> findByTransactionId(String transactionId);
+    boolean existsByAccountNumber(String accountNumber);
 }
