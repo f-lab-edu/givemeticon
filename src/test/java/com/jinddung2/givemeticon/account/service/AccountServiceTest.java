da8234c49053b58cf764a29df92cf2d1492a8503
jinhyuck <gjwlsgur4866@gmail.com>
2023-10-18 18:17:32 +0900

    [#26] test : 계좌 생성 및 유저의 계좌 데이터 등록 테스트

---
+++ src/test/java/com/jinddung2/givemeticon/account/service/AccountServiceTest.java
@@ -0,0 +1,59 @@
+package com.jinddung2.givemeticon.account.service;
+
+import com.jinddung2.givemeticon.account.domain.Account;
+import com.jinddung2.givemeticon.account.dto.request.CreateAccountRequest;
+import com.jinddung2.givemeticon.account.exception.DuplicatedAccountNumberException;
+import com.jinddung2.givemeticon.account.mapper.AccountMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class AccountServiceTest {
+
+    @InjectMocks
+    AccountService accountService;
+
+    @Mock
+    AccountMapper accountMapper;
+
+    CreateAccountRequest createAccountRequest;
+    Account account;
+
+    @BeforeEach
+    void setUp() {
+        createAccountRequest = new CreateAccountRequest("testHolder", "0000", "testBank", "000101");
+        account = Account.builder()
+                .id(100)
+                .accountNumber(createAccountRequest.accountNumber())
+                .accountHolder(createAccountRequest.accountHolder())
+                .bankName(createAccountRequest.bankName())
+                .birth(createAccountRequest.birth())
+                .build();
+    }
+
+    @Test
+    @DisplayName("계좌 등록에 성공한다.")
+    void create_Account_Success() {
+        Mockito.when(accountMapper.save(Mockito.any(Account.class))).thenReturn(account.getId());
+        int id = accountService.create(createAccountRequest);
+
+        Assertions.assertEquals(account.getId(), id);
+    }
+
+    @Test
+    @DisplayName("이미 등록된 계좌 번호라서 계좌 생성에 실패한다.")
+    void create_Account_Fail_Duplicated_Account_Number() {
+        Mockito.when(accountMapper.existsByAccountNumber(createAccountRequest.accountNumber())).thenReturn(true);
+
+        Assertions.assertThrows(DuplicatedAccountNumberException.class,
+                () -> accountService.create(createAccountRequest));
+    }
+
+}
