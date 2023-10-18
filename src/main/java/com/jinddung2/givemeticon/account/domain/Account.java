ad0867671440a46b10129ce701c8c0e50c62c62b
jinhyuck <gjwlsgur4866@gmail.com>
2023-10-18 18:13:52 +0900

    [#26] feat : Account 도메인 수정

--- src/main/java/com/jinddung2/givemeticon/account/domain/Account.java
+++ src/main/java/com/jinddung2/givemeticon/account/domain/Account.java
@@ -1,15 +1,15 @@
 package com.jinddung2.givemeticon.account.domain;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Getter
 public class Account {
     private int id;
-    private String transactionId;
     private String accountHolder;
     private String accountNumber;
-    private String bankCode;
     private String bankName;
     private String birth;
     private LocalDateTime createdDate;
@@ -17,20 +17,16 @@ public class Account {
 
     @Builder
     public Account(int id,
-                   String transactionId,
                    String accountHolder,
                    String accountNumber,
-                   String bankCode,
                    String bankName,
                    String birth,
                    LocalDateTime createdDate,
                    LocalDateTime deletedDate
     ) {
         this.id = id;
-        this.transactionId = transactionId;
         this.accountHolder = accountHolder;
         this.accountNumber = accountNumber;
-        this.bankCode = bankCode;
         this.bankName = bankName;
         this.birth = birth;
         this.createdDate = createdDate;
