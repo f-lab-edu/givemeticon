f402f17cbb37655994d93cae8e769a892aa30c6e
jinhyuck <gjwlsgur4866@gmail.com>
2023-10-19 17:18:59 +0900

    test : 토큰 생성 테스트

---
+++ src/test/java/com/jinddung2/givemeticon/common/security/provider/JwtTokenProviderTest.java
@@ -0,0 +1,40 @@
+package com.jinddung2.givemeticon.common.security.provider;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+class JwtTokenProviderTest {
+
+    @Mock
+    JwtTokenProvider jwtTokenProvider;
+
+    String subject;
+
+    @BeforeEach
+    void setUp() {
+        String secretKey = "my1test2secret3keymy1test2secret3keymy1test2secret3keymy1test2secret3key";
+        jwtTokenProvider = new JwtTokenProvider(secretKey);
+        subject = "testSubject";
+    }
+
+    @Test
+    void generate_Token() {
+        Date expiredTime = new Date(System.currentTimeMillis() + (60 * 60 * 1000));
+
+        String token = jwtTokenProvider.generate(subject, expiredTime);
+
+        assertTrue(token.length() > 0);
+
+        String[] tokenParts = token.split("\\.");
+        assertEquals(3, tokenParts.length);
+    }
+}
