45d590c1ab291a1a34aaf12efa7c98a3f2a1c9b4
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-02-10 16:31:01 +0900

    [#79] feat : 선착순 쿠폰 생성 로직 변경
    1. 락 흭득 레이어 service -> facade
    2. 락 흭득 위치에 따른 메서드명 변경
    * decreaseStockAndGetLock -> decreaseStock

--- src/main/java/com/jinddung2/givemeticon/domain/coupon/facade/CreateCouponFacade.java
+++ src/main/java/com/jinddung2/givemeticon/domain/coupon/facade/CreateCouponFacade.java
@@ -2,32 +2,50 @@ package com.jinddung2.givemeticon.domain.coupon.facade;
 
 import com.jinddung2.givemeticon.domain.coupon.controller.dto.CreateCouponRequestDto;
 import com.jinddung2.givemeticon.domain.coupon.domain.CouponStock;
-import com.jinddung2.givemeticon.domain.coupon.exception.NotEnoughCouponStockException;
 import com.jinddung2.givemeticon.domain.coupon.service.CouponService;
 import com.jinddung2.givemeticon.domain.coupon.service.CouponStockService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.concurrent.TimeUnit;
+
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CreateCouponFacade {
 
+    private final RedissonClient redissonClient;
     private final CouponService couponService;
     private final CouponStockService couponStockService;
 
     @Transactional
-    public int createCouponAndDecreaseStock(int userId, CreateCouponRequestDto requestDto) {
+    public void createCouponAndDecreaseStock(int userId, CreateCouponRequestDto requestDto) {
         CouponStock stock = couponStockService.getStock(requestDto.stockId());
 
-        if (stock.getRemain() <= 0) {
-            throw new NotEnoughCouponStockException();
+        String lockNam = "stockLock:" + stock.getId();
+        RLock lock = redissonClient.getLock(lockNam);
+        String worker = Thread.currentThread().getName();
+
+        try {
+            boolean isLocked = lock.tryLock(1, 3, TimeUnit.SECONDS);
+
+            if (!isLocked) {
+                log.info("락 흭득 실패");
+                return;
+            }
+
+            log.info("working worker: {} & remain stock: {}", worker, stock.getRemain());
+            couponService.createCoupon(
+                    userId, requestDto.stockId(), requestDto.couponName(), requestDto.couponType(), requestDto.price());
+            couponStockService.decreaseStock(stock);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
         }
-
-        int couponId = couponService.createCoupon(
-                userId, requestDto.stockId(), requestDto.couponName(), requestDto.couponType(), requestDto.price());
-        couponStockService.decreaseStockAndGetLock(stock);
-
-        return couponId;
     }
 }
