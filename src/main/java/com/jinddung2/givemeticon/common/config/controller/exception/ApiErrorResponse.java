913115c7d0333fbd290c0a2fb662aae2248166b3
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-05-29 17:15:33 +0900

    [feat] 응답 포맷 수정

---
+++ src/main/java/com/jinddung2/givemeticon/common/config/controller/exception/ApiErrorResponse.java
@@ -0,0 +1,28 @@
+package com.jinddung2.givemeticon.common.config.controller.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class ApiErrorResponse extends RuntimeException {
+    private final String code;
+    private final String message;
+
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private final List<ValidationError> errors;
+
+    public record ValidationError(String field, String message) {
+
+        public static ValidationError of(final FieldError fieldError) {
+            return new ValidationError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
+}
