cd26c2087b65bd411e3e3ffc67e00082719683c9
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-05-29 19:05:59 +0900

    [feat] 외부 통신 예외 처리

--- src/main/java/com/jinddung2/givemeticon/common/config/controller/hanlder/ApiResponseErrorHandler.java
+++ src/main/java/com/jinddung2/givemeticon/common/config/controller/hanlder/ApiResponseErrorHandler.java
@@ -1,6 +1,6 @@
 package com.jinddung2.givemeticon.common.config.controller.hanlder;
 
-import com.jinddung2.givemeticon.common.config.controller.exception.ApiErrorResponse;
+import com.jinddung2.givemeticon.common.exception.ExternalApiRequestException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.ResponseErrorHandler;
@@ -8,7 +8,6 @@ import org.springframework.web.client.ResponseErrorHandler;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -27,7 +26,7 @@ public class ApiResponseErrorHandler implements ResponseErrorHandler {
 
         log.error("API 호출 중 에러 발생: HTTP 상태 코드: {}, 응답 본문: {}", response.getStatusCode().value(), body);
 
-        throw new ApiErrorResponse(response.getStatusCode().toString(),
-                "API 호출 중 에러 발생: " + response.getStatusCode().value() + " 응답 본문: " + body, new ArrayList<>());
+        throw new ExternalApiRequestException(response.getStatusCode().toString(),
+                "API 호출 중 에러 발생: " + response.getStatusCode().value() + " 응답 본문: " + body);
     }
 }
