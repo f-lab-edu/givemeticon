2289cb57cee3bcfdf41434cbb1d9efc643f10b0a
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-05-31 21:01:02 +0900

    [test] UserFixture 생성 시 시간 받기

--- src/test/java/com/jinddung2/givemeticon/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/jinddung2/givemeticon/domain/user/controller/UserControllerTest.java
@@ -37,6 +37,7 @@ import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -86,7 +87,7 @@ public class UserControllerTest {
     GetMyPointFacade getMyPointFacade;
 
     MockHttpSession mockHttpSession;
-    UserDto userDto;
+    LocalDateTime now = LocalDateTime.now();
 
     @BeforeEach
     void setUp() {
@@ -97,7 +98,7 @@ public class UserControllerTest {
     @DisplayName("회원 가입을 성공한다.")
     void signUp_Success() throws Exception {
         SignUpRequest request = new SignUpRequest("test1234@example.com", "test1234", "01012345678");
-        User userFixture = UserFixture.createUserFixture(request.getEmail(), request.getPassword(), request.getPhone());
+        User userFixture = UserFixture.createUserFixture(request.getEmail(), request.getPassword(), request.getPhone(), now);
 
         when(signUpFacade.signUp(request)).thenReturn(userFixture.getId());
 
@@ -149,7 +150,7 @@ public class UserControllerTest {
     @Test
     @DisplayName("사용자 회원 정보 조회에 성공한다.")
     void get_UserInfo_Success() throws Exception {
-        User userFixture = UserFixture.createUserFixture();
+        User userFixture = UserFixture.createUserFixture(now);
         UserDto result = UserDto.of(userFixture);
 
         mockHttpSession.setAttribute(LOGIN_USER, userFixture.getId());
@@ -178,7 +179,7 @@ public class UserControllerTest {
     @Test
     @DisplayName("해당 유저가 존재하지 않아 회원 정보 조회에 실패한다.")
     void get_UserInfo_Fail_Not_Exists() throws Exception {
-        User userFixture = UserFixture.createUserFixture();
+        User userFixture = UserFixture.createUserFixture(now);
         UserDto result = UserDto.of(userFixture);
         mockHttpSession.setAttribute(LOGIN_USER, userFixture.getId());
 
@@ -199,7 +200,7 @@ public class UserControllerTest {
     @DisplayName("로그인을 성공한다.")
     void login_Success() throws Exception {
         LoginRequest request = new LoginRequest("test1234@example.com", "test1234");
-        User userFixture = UserFixture.createUserFixture(request.getEmail(), request.getPassword());
+        User userFixture = UserFixture.createUserFixture(request.getEmail(), request.getPassword(), now);
         UserDto result = UserDto.of(userFixture);
 
         given(userService.checkLogin(request.getEmail(), request.getPassword())).willReturn(result);
@@ -227,7 +228,7 @@ public class UserControllerTest {
     @Test
     @DisplayName("로그아웃을 성공한다.")
     void logout_Success() throws Exception {
-        User userFixture = UserFixture.createUserFixture();
+        User userFixture = UserFixture.createUserFixture(now);
         String responseBody = "Successfully logout";
 
         mockHttpSession.setAttribute(LOGIN_USER, userFixture.getId());
@@ -247,7 +248,7 @@ public class UserControllerTest {
     @Test
     @DisplayName("비밀번호 변경에 성공한다.")
     void update_Password_Success() throws Exception {
-        User userFixture = UserFixture.createUserFixture();
+        User userFixture = UserFixture.createUserFixture(now);
         PasswordUpdateRequest request = new PasswordUpdateRequest("test1234", "newtest1234");
         String responseBody = "Successfully update password";
 
@@ -270,7 +271,7 @@ public class UserControllerTest {
     @Test
     @DisplayName("해당 유저가 존재하지 않아 비밀번호 변경에 실패한다.")
     void update_Password_Fail_Not_Exists_User() throws Exception {
-        User userFixture = UserFixture.createUserFixture();
+        User userFixture = UserFixture.createUserFixture(now);
         PasswordUpdateRequest request = new PasswordUpdateRequest("test1234", "newtest1234");
 
         mockHttpSession.setAttribute(LOGIN_USER, userFixture.getId());
@@ -290,7 +291,7 @@ public class UserControllerTest {
     @Test
     @DisplayName("이전 비밀번호가 일치하지 않아 비밀번호 변경에 실패한다.")
     void update_Password_MisMatch_Password() throws Exception {
-        User userFixture = UserFixture.createUserFixture();
+        User userFixture = UserFixture.createUserFixture(now);
         PasswordUpdateRequest request = new PasswordUpdateRequest("test1234", "newtest1234");
 
         mockHttpSession.setAttribute(LOGIN_USER, userFixture.getId());
@@ -311,7 +312,7 @@ public class UserControllerTest {
     @DisplayName("등록된 이메일에 임시 비밀번호 발급에 성공한다.")
     void send_Temporary_Password() throws Exception {
         PasswordResetRequest request = new PasswordResetRequest("test1234@example.com");
-        User userFixture = UserFixture.createUserFixture();
+        User userFixture = UserFixture.createUserFixture(now);
         String responseBody = "Successfully reset password";
 
         mockHttpSession.setAttribute(LOGIN_USER, userFixture.getId());
@@ -333,7 +334,7 @@ public class UserControllerTest {
     @DisplayName("유저가 계좌 생성하는데 성공한다.")
     void create_Account_Link_User_Account_Id() throws Exception {
         CreateAccountRequest request = new CreateAccountRequest("testHolder", "0000", "testBank", "000101");
-        User userFixture = UserFixture.createUserFixture();
+        User userFixture = UserFixture.createUserFixture(now);
 
         mockHttpSession.setAttribute(LOGIN_USER, userFixture.getId());
         when(createAccountFacade.createAccount(userFixture.getId(), request)).thenReturn(userFixture.getAccountId());
@@ -354,7 +355,7 @@ public class UserControllerTest {
     @DisplayName("계좌번호가 이미 등록되어서 유저와 계좌 연결에 실패한다.")
     void create_Account_Fail_Duplicated_Account_Number() throws Exception {
         CreateAccountRequest request = new CreateAccountRequest("testHolder", "0000", "testBank", "000101");
-        User userFixture = UserFixture.createUserFixture();
+        User userFixture = UserFixture.createUserFixture(now);
 
         mockHttpSession.setAttribute(LOGIN_USER, userFixture.getId());
         doThrow(new DuplicatedAccountNumberException())
@@ -375,7 +376,7 @@ public class UserControllerTest {
     @Test
     @DisplayName("특정 상품에 좋아요를 누르는데 성공한다.")
     void push_Favorite_Item() throws Exception {
-        User userFixture = UserFixture.createUserFixture();
+        User userFixture = UserFixture.createUserFixture(now);
         Item itemFixture = ItemFixture.createItemFixture();
         String responseBody = "Successfully like item " + itemFixture.getId();
 
@@ -397,7 +398,7 @@ public class UserControllerTest {
     @Test
     @DisplayName("상품에 좋아요를 눌렀지만 이미 좋아요 한 상품이라 실패한다.")
     void push_Favorite_Item_Fail_Already_Item_Favorite() throws Exception {
-        User userFixture = UserFixture.createUserFixture();
+        User userFixture = UserFixture.createUserFixture(now);
         Item itemFixture = ItemFixture.createItemFixture();
 
         mockHttpSession.setAttribute(LOGIN_USER, userFixture.getId());
@@ -417,7 +418,7 @@ public class UserControllerTest {
     @Test
     @DisplayName("좋아요를 누른 상품을 취소하는 것을 확인한다.")
     void cancel_Favorite_Item() throws Exception {
-        User userFixture = UserFixture.createUserFixture();
+        User userFixture = UserFixture.createUserFixture(now);
         Item itemFixture = ItemFixture.createItemFixture();
         String responseBody = "Successfully cancel like item: " + itemFixture.getId();
 
@@ -439,7 +440,7 @@ public class UserControllerTest {
     @Test
     @DisplayName("좋아요 한 적이 없는 상품이라 좋아요 취소에 실패한다.")
     void cancel_Favorite_Item_Fail_Not_Push_Item_Favorite() throws Exception {
-        User userFixture = UserFixture.createUserFixture();
+        User userFixture = UserFixture.createUserFixture(now);
         Item itemFixture = ItemFixture.createItemFixture();
 
         mockHttpSession.setAttribute(LOGIN_USER, userFixture.getId());
@@ -459,7 +460,7 @@ public class UserControllerTest {
     @Test
     @DisplayName("내가 좋아요한 상품들을 조회하는데 성공한다.")
     void get_My_Favorite_Items() throws Exception {
-        User userFixture = UserFixture.createUserFixture();
+        User userFixture = UserFixture.createUserFixture(now);
         Item itemFixture1 = ItemFixture.createItemFixture(30);
         Item itemFixture2 = ItemFixture.createItemFixture(10);
         Item itemFixture3 = ItemFixture.createItemFixture(20);
@@ -491,7 +492,7 @@ public class UserControllerTest {
     @Test
     @DisplayName("내 포인트 조회 api가 성공한다.")
     void get_my_point() throws Exception {
-        User userFixture = UserFixture.createUserFixture();
+        User userFixture = UserFixture.createUserFixture(now);
         int myPoint = 10000;
 
         mockHttpSession.setAttribute(LOGIN_USER, userFixture.getId());
@@ -511,20 +512,21 @@ public class UserControllerTest {
     @Test
     @DisplayName("캐시 포인트 데이터가 존재하지 않아 포인트 조회 api가 실패한다.")
     void get_my_point_fail_not_found_cash_point() throws Exception {
-        User userFixture = UserFixture.createUserFixture();
+        User userFixture = UserFixture.createUserFixture(now);
 
         mockHttpSession.setAttribute(LOGIN_USER, userFixture.getId());
         doThrow(new NotFoundCashPoint())
                 .when(getMyPointFacade).getMyPoint(userFixture.getId());
 
         mockMvc.perform(MockMvcRequestBuilders
-                .get("/api/v1/users/my-point")
-                .contentType(MediaType.APPLICATION_JSON)
-                .session(mockHttpSession))
+                        .get("/api/v1/users/my-point")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .session(mockHttpSession))
                 .andExpect(status().is4xxClientError())
                 .andExpect(jsonPath("$.code").value(CashPointErrorCode.NOT_FOUND_CASH_POINT.getHttpStatus().value()))
                 .andExpect(jsonPath("$.message").value(CashPointErrorCode.NOT_FOUND_CASH_POINT.getHttpStatus().name()))
-                .andExpect(jsonPath("$.errorDetail").value(CashPointErrorCode.NOT_FOUND_CASH_POINT.getErrorDetail()));;
+                .andExpect(jsonPath("$.errorDetail").value(CashPointErrorCode.NOT_FOUND_CASH_POINT.getErrorDetail()));
+        ;
 
     }
 }
