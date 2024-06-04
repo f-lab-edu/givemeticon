e64cbecd12d428f18cd5a0ce6874f1ddcae3e0dd
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-06-04 16:08:56 +0900

    [feat] add trade, sale fixture

---
+++ src/test/java/com/jinddung2/givemeticon/fixture/SaleFixture.java
@@ -0,0 +1,28 @@
+package com.jinddung2.givemeticon.fixture;
+
+import com.jinddung2.givemeticon.domain.item.domain.Item;
+import com.jinddung2.givemeticon.domain.sale.domain.Sale;
+import com.jinddung2.givemeticon.domain.user.domain.User;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class SaleFixture {
+
+    public static Sale createSaleFixture(User buyer, Item item) {
+        LocalDateTime nowTime = LocalDateTime.now();
+        LocalDate nowDate = LocalDate.of(nowTime.getYear(), nowTime.getMonth(), nowTime.getDayOfMonth()).plusMonths(1);
+        return Sale.builder()
+                .id(6)
+                .itemId(item.getId())
+                .sellerId(buyer.getId())
+                .barcode("012345678901")
+                .expirationDate(nowDate.plusMonths(1))
+                .isBought(false)
+                .isBoughtDate(Date.valueOf(nowDate))
+                .createdDate(nowTime)
+                .updatedDate(nowTime)
+                .build();
+    }
+}
---
+++ src/test/java/com/jinddung2/givemeticon/fixture/TradeFixture.java
@@ -0,0 +1,28 @@
+package com.jinddung2.givemeticon.fixture;
+
+import com.jinddung2.givemeticon.domain.item.domain.Item;
+import com.jinddung2.givemeticon.domain.sale.domain.Sale;
+import com.jinddung2.givemeticon.domain.trade.domain.Trade;
+import com.jinddung2.givemeticon.domain.user.domain.User;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class TradeFixture {
+
+    public static Trade createTradeFixture(User buyer, Sale sale, Item item) {
+        LocalDate now = LocalDate.now();
+        Trade trade = Trade.builder()
+                .id(7)
+                .buyerId(buyer.getId())
+                .saleId(sale.getId())
+                .tradePrice(BigDecimal.valueOf(item.getPrice()))
+                .isUsed(false)
+                .isUsedDate(null)
+                .createdDate(now)
+                .build();
+
+        trade.discountItemPrice(0.05);
+        return trade;
+    }
+}
