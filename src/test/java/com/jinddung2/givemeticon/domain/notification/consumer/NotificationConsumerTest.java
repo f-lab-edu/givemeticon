56c2c3d838459f76e7bc1159cf87f3376467bfd0
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-06-11 17:41:24 +0900

    [test] notification 테스트 리팩토링 (#104)

--- src/test/java/com/jinddung2/givemeticon/domain/notification/consumer/NotificationConsumerTest.java
+++ src/test/java/com/jinddung2/givemeticon/domain/notification/consumer/NotificationConsumerTest.java
@@ -16,24 +16,22 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class NotificationConsumerTest {
 
     @InjectMocks
-    NotificationConsumer notificationConsumer;
+    NotificationConsumer sut;
 
     @Mock
     NotificationMapper notificationMapper;
 
-    int saleId = 1;
-    int sellerId = 2;
-    int notificationId= 3;
     @Test
     @DisplayName("알람 요청을 consume 해서 DB에 저장한다.")
     void consume() {
+        int saleId = 1, sellerId = 2, notificationId= 3;
         CreateNotificationRequestDto requestFakeDto = new CreateNotificationRequestDto(saleId, sellerId, "fakeMessage");
         ConsumerRecord<String, CreateNotificationRequestDto> record =
                 new ConsumerRecord<>("fakeTopic", 0, 0, "fakeKey", requestFakeDto);
 
         Mockito.when(notificationMapper.save(Mockito.any(Notification.class))).thenReturn(notificationId);
 
-        notificationConsumer.consume(record);
+        sut.consume(record);
 
         Mockito.verify(notificationMapper).save(Mockito.any(Notification.class));
     }
