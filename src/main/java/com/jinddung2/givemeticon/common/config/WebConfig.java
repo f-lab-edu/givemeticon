08b63bda3c50fe3f16a434ca705465a80f1e7b78
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-06-12 14:51:51 +0900

    [feat] 로그인 검사 제외할 url 코드 AuthInterceptor -> WebConfig 이동

--- src/main/java/com/jinddung2/givemeticon/common/config/WebConfig.java
+++ src/main/java/com/jinddung2/givemeticon/common/config/WebConfig.java
@@ -13,20 +13,28 @@ public class WebConfig implements WebMvcConfigurer {
     private final AuthInterceptor authInterceptor;
 
     private final String[] EXCLUDE_PATH = {
+            "/favicon.ico",
+            "/error",
             "/api/v1/users/sign-up",
             "/api/v1/users/login",
             "/api/v1/mails/send-certification",
             "/api/v1/mails/verify",
             "/auth/*/callback",
             "/api/v1/auth/**",
-            "/favicon.ico",
-            "/error"
+            "/api/v1/auth/**",
+            "/api/v1/brands/category/**",
+            "/api/v1/brands/**",
+            "/api/v1/brands/category",
+            "/api/v1/categories",
+            "/api/v1/items/**",
+            "/api/v1/sales/*",
+            "/api/v1/sales/items/*"
     };
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns(EXCLUDE_PATH);
+                .excludePathPatterns(EXCLUDE_PATH)
+                .addPathPatterns("/**");
     }
 }
--- src/main/java/com/jinddung2/givemeticon/common/security/interceptor/AuthInterceptor.java
+++ src/main/java/com/jinddung2/givemeticon/common/security/interceptor/AuthInterceptor.java
@@ -5,9 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
-import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
@@ -17,48 +15,11 @@ public class AuthInterceptor implements HandlerInterceptor {
 
     private final LoginService loginService;
 
-    private final String[] ALLOW_GET_PATH = {
-            "/api/v1/brands/category/**",
-            "/api/v1/brands/**",
-            "/api/v1/brands/category",
-            "/api/v1/categories",
-            "/api/v1/items/**",
-            "/api/v1/sales/**",
-            "/api/v1/sales/items/**",
-    };
-
-    private final String[] ALLOW_POST_PATH = {
-            "/api/v1/auth/**"
-    };
-
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
         log.debug("Login Interceptor preHandler");
 
-        if (checkAllowUrl(request)) return true;
-
         return sessionLoginValidate();
-    }
-
-    private boolean checkAllowUrl(HttpServletRequest request) {
-        String requestURI = request.getRequestURI();
-
-        if (request.getMethod().equals(HttpMethod.GET.name())) {
-            return isPathAllowed(ALLOW_GET_PATH, requestURI);
-        } else if (request.getMethod().equals(HttpMethod.POST.name())) {
-            return isPathAllowed(ALLOW_POST_PATH, requestURI);
-        }
-        return false;
-    }
-
-    private boolean isPathAllowed(String[] allowedPaths, String requestURI) {
-        AntPathMatcher antPathMatcher = new AntPathMatcher();
-        for (String allowPath : allowedPaths) {
-            if (antPathMatcher.match(allowPath, requestURI)) {
-                return true;
-            }
-        }
-        return false;
     }
 
     private boolean sessionLoginValidate() {
