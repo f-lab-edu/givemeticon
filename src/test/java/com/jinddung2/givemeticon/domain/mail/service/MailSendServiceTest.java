fe216b3d5b315abaf897cd4da3844131a4a95797
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-06-12 15:34:40 +0900

    [Test] /#101 mail (#105)
    * [feat] 로그인 검사 제외할 url 코드 AuthInterceptor -> WebConfig 이동
    
    * [test] mail test 리팩토링

--- src/test/java/com/jinddung2/givemeticon/domain/mail/service/MailSendServiceTest.java
+++ src/test/java/com/jinddung2/givemeticon/domain/mail/service/MailSendServiceTest.java
@@ -6,26 +6,25 @@ import com.jinddung2.givemeticon.domain.mail.config.MailCustomProperties;
 import com.jinddung2.givemeticon.domain.mail.repository.CertificationNumberDao;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import java.security.NoSuchAlgorithmException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class MailSendServiceTest {
 
     @InjectMocks
-    MailSendService mailSendService;
+    MailSendService sut;
+
     @Mock
     JavaMailSender mailSender;
 
@@ -44,12 +43,6 @@ class MailSendServiceTest {
     @Mock
     MailCustomProperties properties;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-        mailSendService = new MailSendService(mailSender, certificationNumberDao, passwordGenerator, certificationGenerator, properties);
-    }
-
     @Test
     @DisplayName("회원가입 시 해당 이메일로 인증코드 6자리 숫자 보내기")
     void send_Email_Certification() throws NoSuchAlgorithmException, MessagingException {
@@ -64,7 +57,7 @@ class MailSendServiceTest {
         doNothing().when(mailSender).send(any(MimeMessage.class));
         // when
         when(properties.getMailTitleCertification()).thenReturn(mailTitleCertification);
-        mailSendService.sendEmailForCertification(email);
+        sut.sendEmailForCertification(email);
 
         verify(certificationGenerator).createCertificationNumber();
         verify(mailSender).send(any(MimeMessage.class));
@@ -72,25 +65,22 @@ class MailSendServiceTest {
     }
 
     @Test
-    @DisplayName("회원가입 시 해당 이메일로 임시 비밀번호 12자리 숫자 보내기")
-    void send_Email_Temp_Password() throws MessagingException {
-        // given
+    @DisplayName("회원가입 시 해당 이메일로 임시 비밀번호 12자리 숫자를 보낸다.")
+    void sendEmailTempPassword_shouldSendTempPassword() throws MessagingException {
+
         String email = "test@example.com";
         String tempPassword = "0123456789ab";
         String mailTitleCertification = "givemeticon 인증번호 안내";
 
         when(passwordGenerator.createTemporaryPassword()).thenReturn(tempPassword);
         when(mailSender.createMimeMessage()).thenReturn(mimeMessage);
-
         doNothing().when(mailSender).send(any(MimeMessage.class));
-        // when
         when(properties.getMailTitleCertification()).thenReturn(mailTitleCertification);
-        String response = mailSendService.sendEmailForTemporaryPassword(email);
+
+        String result = sut.sendEmailForTemporaryPassword(email);
 
         verify(passwordGenerator).createTemporaryPassword();
         verify(mailSender).send(any(MimeMessage.class));
-
-        assertEquals(tempPassword, response);
+        assertThat(result).isEqualTo(tempPassword);
     }
-
 }
