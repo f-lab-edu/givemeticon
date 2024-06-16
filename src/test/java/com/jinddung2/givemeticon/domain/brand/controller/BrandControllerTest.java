3d97d8947ff6cbcecd02991c3a5295fe61c6a152
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-06-16 14:49:42 +0900

    [test] 중복 제거

--- src/test/java/com/jinddung2/givemeticon/domain/brand/controller/BrandControllerTest.java
+++ src/test/java/com/jinddung2/givemeticon/domain/brand/controller/BrandControllerTest.java
@@ -20,7 +20,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.List;
@@ -31,9 +30,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(value = BrandController.class)
 class BrandControllerTest extends BasicControllerTest {
-
-    @Autowired
-    MockMvc mockMvc;
 
     @Autowired
     ObjectMapper objectMapper;
