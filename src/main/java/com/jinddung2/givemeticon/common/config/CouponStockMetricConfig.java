41000c7e3f8259d40a9924f936e29d6badea8251
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2025-01-16 16:49:05 +0900

    [refactor] 빈 등록으로 수정

--- src/main/java/com/jinddung2/givemeticon/common/config/CouponStockMetricConfig.java
+++ src/main/java/com/jinddung2/givemeticon/common/config/CouponStockMetricConfig.java
@@ -3,9 +3,9 @@ package com.jinddung2.givemeticon.common.config;
 import com.jinddung2.givemeticon.domain.coupon.domain.CouponStock;
 import com.jinddung2.givemeticon.domain.coupon.service.CouponStockService;
 import io.micrometer.core.instrument.Gauge;
-import io.micrometer.core.instrument.MeterRegistry;
-import jakarta.annotation.PostConstruct;
+import io.micrometer.core.instrument.binder.MeterBinder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
@@ -14,23 +14,24 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CouponStockMetricConfig {
 
-    private final MeterRegistry registry;
-    private final CouponStockService couponStockService;
+    @Bean
+    public MeterBinder stockSize(CouponStockService couponStockService) {
+        return registry -> {
+            // 활성 쿠폰 재고 목록 가져오기
+            List<CouponStock> activeCouponStockIds = couponStockService.getActiveCouponStocks();
 
-    @PostConstruct
-    public void initCouponStockGauge() {
-        List<CouponStock> activeCouponStockIds = couponStockService.getActiveCouponStockIds();
+            for (CouponStock mapping : activeCouponStockIds) {
+                int couponStockId = mapping.getId();
 
-        for (CouponStock mapping : activeCouponStockIds) {
-            int couponStockId = mapping.getId();
-
-            Gauge.builder("coupon.stock.current",
-                            couponStockService,
-                            service -> service.getStock(couponStockId).getRemain())
-                    .tag("couponId", String.valueOf(couponStockId))
-                    .description("쿠폰별 남은 재고 모니터링")
-                    .register(registry);
-        }
+                // 각 쿠폰 ID에 대한 Gauge 등록
+                Gauge.builder("coupon.stock.current",
+                                couponStockService,
+                                service -> service.getStock(couponStockId).getRemain())
+                        .tag("couponId", String.valueOf(couponStockId))
+                        .description("쿠폰별 남은 재고 모니터링")
+                        .register(registry);
+            }
+        };
     }
 
 }
